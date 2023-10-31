7b3b91bd54b96d9c546ed7f15b7d85b9fdeaa96c
Willy <al.guillermobermell@iesramonllull.net>
2023-10-31 13:04:41 +0100

    mi commit 1.2

---
+++ prueba.py
@@ -0,0 +1,42 @@
+print("Hola Mundo")
+#soy un comentario
+
+"""
+Texto
+"""
+#variables
+texto = "Repaso de python"
+nombre = "willy"
+year = 2023
+
+print(f"{texto} - {nombre} - {str(year)}")
+
+# Entrada
+#sitioweb = input("¿Cuál es tu página web?:")
+#print("El sitio web del usuario es: " + sitioweb)
+
+# Condiciones
+"""
+altura = int(input("¿Cuál es tu altura?: "))
+if altura >= 170:
+    print("Muy bien")
+else:
+    print("Eres bajito")
+"""
+
+# Funciones
+
+var_altura = int(input("¿Cuál es tu altura?: "))
+
+def mostrarAltura(altura):
+    resultado = ""
+
+    if altura >= 170:
+         resultado = "Muy bien"
+    else:
+         resultado = ("Eres bajito")
+    
+    return resultado
+
+mostrarAltura(var_altura)
+mostrarAltura(var_altura)
